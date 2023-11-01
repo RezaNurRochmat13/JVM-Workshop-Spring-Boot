1c08d034fae12025915dd93588c1f06bfd925e6f
Reja <rezanurrochmat@gmail.com>
2023-11-01 11:36:40 +0700

    upgrade spring boot to 3xx and fix error after upgrade

--- src/main/java/com/backend/jvmworkshop/module/spareparts/domain/Spareparts.java
+++ src/main/java/com/backend/jvmworkshop/module/spareparts/domain/Spareparts.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Data;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "spareparts")
